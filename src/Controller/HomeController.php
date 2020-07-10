52ec85f710e86997dab18a4cabd4a593c2181d6b
Reapex <tr7296228@gmail.com>
2020-07-10 14:28:23 +0200

    Update Packages

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CommentFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/")
+     */
+    public function redirectHome()
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'fr']);
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/", name="homepage")
+     */
+    public function test()
+    {
+        return $this->render('base.html.twig');
+    }
+}
