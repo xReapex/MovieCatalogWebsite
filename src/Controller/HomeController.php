b51c5a8939aa357ca7818ba3d1af6cdd16fb36d8
Reapex <tr7296228@gmail.com>
2020-07-15 16:07:51 +0200

    Giga update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
 use App\Form\CommentFormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Twig\Environment;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\UserRepository;
 
 class HomeController extends AbstractController
 {
-
     private $twig;
+    private $repository;
 
     public function __construct(Environment $twig)
     {
@@ -28,9 +40,138 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/{_locale<%app.supported_locales%>}/", name="homepage")
+     * @return Response
      */
-    public function test()
+    public function showHome()
     {
-        return $this->render('base.html.twig');
+        return $this->render('home/show_home.html.twig');
     }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/user/register", name="register_user")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function createUser(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(FormType::class, $user);
+
+        $form
+            ->add('Username', TextType::class)
+            ->add('Email', EmailType::class)
+            ->add('Password', PasswordType::class)
+            ->add('Submit', SubmitType::class);
+
+        if ($request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $hash = $encoder->encodePassword($user, $user->getPassword());
+
+                $user->setPassword($hash);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                return $this->redirectToRoute('user_show');
+            }
+        }
+
+        return $this->render('user/form.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/user/{id}", name="user_show_id")
+     * @param $id
+     * @param UserRepository $userRepository
+     * @return Response
+     */
+    public function showID($id, UserRepository $userRepository)
+    {
+        $user = $userRepository->find($id);
+
+        if (!$user){
+            throw $this->createNotFoundException(
+                'Pas d\'utilisateur avec cet id !'.$id
+            );
+        }
+
+        return new Response("Utilisateur trouvé: ".$user->getUsername());
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/user", name="user_show")
+     * @param UserRepository $userRepository
+     * @return Response
+     */
+    public function showUser(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('user/user.html.twig', [
+            "users" => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/user/delete/{id}", name="user_delete")
+     * @param $id
+     * @param UserRepository $userRepository
+     * @return RedirectResponse
+     */
+    public function deleteUser($id, UserRepository $userRepository)
+    {
+        $user = $userRepository->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('user_show');
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/user/edit/{id}", name="user_edit")
+     * @param Request $request
+     * @param User $user
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return RedirectResponse|Response
+     */
+    public function editArticle(Request $request, User $user, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $form = $this->createForm(FormType::class, $user);
+
+        $form
+            ->add('Username', TextType::class)
+            ->add('Email', EmailType::class)
+            ->add('Password', PasswordType::class)
+            ->add('Submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $encoded = $encoder->encodePassword($user, $form->get('Password')->getData());
+            $user->setPassword($encoded);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('user_show');
+        }
+
+        return $this->render(
+            'user/form.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
 }
