98222cab30deff5b7666cd65f8cbe583d5081029
Reapex <tr7296228@gmail.com>
2020-07-20 14:45:53 +0200

    Initialize feedback functions

--- src/Controller/Feedback/FeedbackController.php
+++ src/Controller/Feedback/FeedbackController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller\Feedback;
 
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Feedback;
 
 class FeedbackController extends AbstractController
 {
+
     /**
      * @Route("/{_locale<%app.supported_locales%>}/feedback", name="feedback")
      */
@@ -14,4 +21,6 @@ class FeedbackController extends AbstractController
     {
         return $this->render('feedback/index.html.twig');
     }
+
+
 }
