fde3fdc18397331d81e2cc470fb7596339552f25
Reapex <tr7296228@gmail.com>
2020-08-21 11:41:13 +0200

    Update

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -8,9 +8,7 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use VfacTmdb\Factory;
-use VfacTmdb\Search;
-use VfacTmdb\Item;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class FilmController extends AbstractController
 {
@@ -23,17 +21,10 @@ class FilmController extends AbstractController
 
     public function discoverFilm(FilmRepository $films)
     {
-        $tmdb = Factory::create()->getTmdb('e3ff3545d663f593379a9b36980989d8');
-        $item = new Item($tmdb);
-        $responses = $item->getMovie('11', array('language' => 'fr-FR'));
-
-        foreach ($responses as $response)
-        {
-            echo $response->getTitle();
-        }
-
-
-        return $this->render('film/discover.html.twig');
+        $res = $films->discover();
+        return $this->render('film/discover.html.twig', [
+            'films' => $res
+        ]);
     }
 
 }
